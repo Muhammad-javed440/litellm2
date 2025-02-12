247c1366390773bf9a353197e27d77dc705f392b
Muhammad Javed <141915304+Muhammad-javed440@users.noreply.github.com>
2025-02-12 13:20:46 +0500

    Update hello.py

--- src/litellm2/hello.py
+++ src/litellm2/hello.py
@@ -2,7 +2,7 @@ from litellm import completion
 import os
 
 ## set ENV variables
-os.environ["gemini_API_KEY"] = "AIzaSyDkK_3dx9IafgTQzuPn9Ov18zqfr9PT_Zo"
+os.environ["gemini_API_KEY"] = "your api key"
 def call_gemini():
     response = completion(
         model="gemini/gemini-1.5-flash",
